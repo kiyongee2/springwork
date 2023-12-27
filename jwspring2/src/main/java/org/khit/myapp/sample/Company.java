afa3031e530ab8dc2f3863e2519ba39fee382002
kiyongee2 <kiyongee2@gmail.com>
2023-12-27 15:22:32 +0900

    spring test

---
+++ jwspring2/src/main/java/org/khit/myapp/sample/Company.java
@@ -0,0 +1,9 @@
+package org.khit.myapp.sample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Company {
+	
+	private Employee employee;
+}
---
+++ jwspring2/src/main/java/org/khit/myapp/sample/Employee.java
@@ -0,0 +1,12 @@
+package org.khit.myapp.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor  //기본 생성자(매개변수가 없는 생성자)
+@Component  //@Controller, @Repository, @Service의 부모
+public class Employee {
+  
+}
